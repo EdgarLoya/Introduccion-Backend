5d05ef7042d2e420c096200271f694f6f7aa91b3
EdgarLoya <57698330+EdgarLoya@users.noreply.github.com>
2022-04-26 13:01:35 -0600

    este index muestra el texto en forma plana

---
+++ index.js
@@ -0,0 +1,19 @@
+//llamamos el modulo http
+const http = require('http');
+//Establecemos la url o IP de nuestro servidor
+const hostname = '192.168.50.180';
+//establecemos el puerto de escuca
+const port = 3000;
+//creamos una instancia HTTP con reques y un response
+const server = http.createServer((req, res) => {
+    //el servidor respondera un codigo 200
+    res.statusCode = 200;
+    //el servidor respondera con un texto plano
+    res.setHeader('Content-Type', 'text/plain');
+    //El servidor respondera el mensaje hola mundo
+res.end('Hola mundo\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`El servior se esta ejecutando en http://${hostname}:${port}/`);
+});
