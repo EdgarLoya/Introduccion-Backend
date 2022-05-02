1b4076115bf72edfbe11ce7e960614b38b3e96c0
EdgarLoya <57698330+EdgarLoya@users.noreply.github.com>
2022-05-02 11:32:21 -0600

    agregando lo que falto del ultimo commit

--- servidor.js
+++ servidor.js
@@ -30,7 +30,7 @@ http.createServer(function(request, response){
             }
             else {
                 response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: ')+error.code();
+                response.end('Sorry, check with the site admin for error: '+error.code+'..\n');
                 response.end();
             }
         }
